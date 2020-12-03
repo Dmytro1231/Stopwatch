90f64d1334f56a3401bc1de75425c97777ea33e3
Dmytro1231 <dimarubalchenk@ukr.net>
2020-12-03 22:48:11 +0200

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Directive,HostListener  } from '@angular/core';
+import { Button } from 'protractor';
+import { interval, fromEvent, observable, Subscription, timer } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
   title = 'stopwatch';
+
+
+  interval;
+  time = new Date(null);
+
+  startTimer() {
+    this.interval = setInterval(() => {
+      this.time.setSeconds(this.time.getSeconds() + 1);
+    },1000);
+  }
+
+  pauseTimer() {
+    clearInterval(this.interval);
+  }
+
+  resetTimer() {
+    this.time.setSeconds(0);
+  }
 }
